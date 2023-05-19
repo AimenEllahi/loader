522d4dee03faa51eeec4adc3e7b54a2017e25259
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2023-05-19 17:24:18 +0500

    Perfect

--- src/Component/LoadingPage.jsx
+++ src/Component/LoadingPage.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import TranslateIcon from '@mui/icons-material/Translate';
-import LanguageIcon from '@mui/icons-material/Language';
-import PublicIcon from '@mui/icons-material/Public';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import TranslateIcon from "@mui/icons-material/Translate";
+import LanguageIcon from "@mui/icons-material/Language";
+import PublicIcon from "@mui/icons-material/Public";
+import axios from "axios";
 import "./index.css";
 
 const LoadingPage = () => {
@@ -14,76 +14,77 @@ const LoadingPage = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const downloadProgressHandler = (progressEvent) => {
+    const { loaded, total } = progressEvent;
+    const progress = Math.round((loaded / total) * 100);
+    console.log(`Download Progress: ${progress}%`);
+  };
 
   useEffect(() => {
     const fetchLoadingTime = async () => {
       try {
         const startTime = performance.now();
 
-        const response = await axios.get('https://api.publicapis.org/entries');
+        const response = await axios.get("https://api.publicapis.org/entries", {
+          onDownloadProgress: downloadProgressHandler,
+        });
 
         const endTime = performance.now();
         const loadingTime = endTime - startTime;
 
         setLoadingTime(loadingTime);
-        console.log('Loading time:', loadingTime);
+        console.log("Loading time:", loadingTime);
       } catch (error) {
-        console.error('Error fetching loading time:', error);
+        console.error("Error fetching loading time:", error);
       }
     };
 
     fetchLoadingTime();
   }, []);
 
-  const animationDelay = `${(loadingTime / 1000) * 80}%`; // Convert loading time to animation delay percentage
-
   return (
     <div className='icon-container'>
       <span
-        className="text text-e"
+        className='text text-e'
         style={{
           fontSize: windowWidth < 800 ? 50 : 100,
           fontFamily: "bold",
-          animationDelay: animationDelay,
         }}
       >
         E
       </span>
       <span
-        className="text text-l"
+        className='text text-l'
         style={{
           fontSize: windowWidth < 800 ? 50 : 100,
           fontFamily: "bold",
-          animationDelay: animationDelay,
         }}
       >
         L
       </span>
       <TranslateIcon
-        className="icon translate"
+        className='icon translate'
         style={{
           fontSize: windowWidth < 800 ? 50 : 100,
-          animationDelay: animationDelay,
         }}
       />
       <LanguageIcon
-        className="icon language"
+        className='icon language'
         style={{
           fontSize: windowWidth < 800 ? 50 : 100,
-          animationDelay: animationDelay ,
         }}
       />
       <PublicIcon
-        className="icon public"
+        className='icon public'
         style={{
           fontSize: windowWidth < 800 ? 50 : 100,
-          animationDelay: animationDelay,
         }}
       />
     </div>
